d119f237baab52ed1bd43fca9fb730bf0096a11e
David Matalik <david.matalik@posteo.de>
2021-08-30 10:32:45 +0200

    Add content and style to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,20 @@
+import './Home.css'
+import { NavLink } from 'react-router-dom'
+import cart from '../shopping-cart.svg'
+
 const Home = () => {
   return (
-    <h1>This is Home</h1>
+    <section>
+      <h1>This is Home</h1>
+      <p className='p-home'>Welcome to this shop!</p>
+      <p className='p-home'>
+        Shop whatever you like but don't shop more than you need. Instead of
+        shopping brandnew products also consider shopping second hand articles.
+      </p>
+      <NavLink to='/shop'>
+        <img className='cart-img' src={cart} alt='Shopping cart' />
+      </NavLink>
+    </section>
   )
 }
 
